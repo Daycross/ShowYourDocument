4098c3d6246c2de33bf6e5a563e86a4fd0a4736c
Sapucaia <lulisapucaia39@gmail.com>
2022-02-25 12:53:56 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import { exportImages } from '../services/ExportImages'
+
+import '../styles/home.scss'
+
+type targetProps = {
+  target: HTMLInputElement
+}
+
+export function Home(){
+  const [selectedFile, setSelectedFile] = useState<File | undefined>();
+	const [isFilePicked, setIsFilePicked] = useState(false);
+  const images = exportImages();
+
+	// 	setSelectedFile(event.target.value);
+	// 	setIsFilePicked(true);
+
+	// };
+
+  return(
+    <div id="general">
+      <aside>
+        <img src={images[0]} alt="" />
+      </aside>
+      <main className="mainContent">
+        <div className="mainContent-introduction">
+          <div className="text">
+            <h2>Tipificação e Extração de dados</h2>
+            <p>
+              Escolha uma das imagens exemplo abaixo ou envie seus próprios 
+              arquivos. Dentro de alguns segundos você terá seu documento ajustado, 
+              tipificado e com todas as informações extraídas.
+            </p>
+            <p> Teste agora :) </p>
+          </div>
+          <img src={images[1]} alt="Imagem de exemplo dos documentos brasileiros" />
+        </div>
+
+        <div className="mainContent-carousel">
+        <Carousel autoPlay={true} infiniteLoop={true}>
+            <div>
+              <img src={images[1]} alt="Imagem de exemplo dos documentos brasileiros" />
+            </div>
+            <div>
+              <img src={images[1]} alt="Imagem de exemplo dos documentos brasileiros" />
+            </div>
+            <div>
+              <img src={images[1]} alt="Imagem de exemplo dos documentos brasileiros" />
+            </div>
+            <div>
+              <img src={images[1]} alt="Imagem de exemplo dos documentos brasileiros" />
+            </div>
+          </Carousel>
+        </div>
+
+        <div className="mainContent-showData">
+          <div className="showData-doc">
+            <h3>Documento</h3>
+            <img src="#" alt="imagem do documento" />
+          </div>
+          <div className="showData-info">
+            <h3>Dados</h3>
+            <div className="showData-infoJSON">
+              <p>Dados Aqui</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mainContent-uploadImage">
+          <h3>Teste com seus arquivos</h3>
+            <label htmlFor="files" className="mainContent-uploadImage_content">
+              <img src={images[7]} alt="Ícone de Upload" />
+              <h3>Clique ou arraste os arquivos aqui</h3>
+              <input id='files' accept=".png, .jpg, .jpeg" type='file' onChange={(event: targetProps) => {
+                if(event.target.files){
+                  console.log('Peguei a imagem')
+                  setSelectedFile(event.target.files[0]);
+                }
+              }}/>
+            </label>          
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ src/services/ExportImages.tsx
@@ -0,0 +1,16 @@
+import foxFlyLogo from '../assets/images/foxFly-logo.png'
+import headerDoc from '../assets/images/headerDoc.png'
+import cpfImage from '../assets/images/cpfImage.png'
+import rgImage from '../assets/images/rgImage.png'
+import rgSecondaryImage from '../assets/images/rgSecondaryImage.png'
+import leftArrow from '../assets/images/leftArrow.svg'
+import rightArrow from '../assets/images/rightArrow.svg'
+import uploadIcon from '../assets/images/uploadIcon.svg'
+
+export function exportImages(){
+  const imagesArray = [
+    foxFlyLogo, headerDoc, cpfImage, rgImage, rgSecondaryImage, leftArrow, rightArrow, uploadIcon
+  ]
+
+  return imagesArray;
+}
